3f86169220284e902661679ed38cfaaac1a93e6d
Sofi Majeed <majeed.sofi@jungleworks.com>
2023-05-27 15:35:20 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const expressJwt = require("express-jwt")
+const PORT = process.env.PORT || 5000
+
+app.use(morgan("dev"))
+app.use(express.json())
+
+//connect to db
+mongoose.connect(
+    `mongodb://${process.env.BASE_URL}/todo-auth-example`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    (err) => {
+        if (err) throw err
+        console.log("Connected to the database")
+    }
+)
+app.use(
+    "/api",
+    expressJwt({ secret: process.env.SECRET, algorithms: ["HS256"] })
+)
+app.use("/auth", require("./routes/auth"))
+app.use("/api/todo", require("./routes/todo"))
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    if (err.name === "UnauthorizedError") {
+        res.status(err.status)
+    }
+    return res.send({ message: err.message })
+})
+
+app.listen(PORT, () => {
+    console.log(`Starting server on port ${PORT}`)
+})
---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const User = require("../models/user");
+const authRouter = express.Router();
+const jwt = require("jsonwebtoken");
+
+authRouter.post("/signup", (req, res, next) => {
+    User.findOne({ username: req.body.username }, (err, existingUser) => {
+        if (err) {
+            res.status(500);
+            return next(err);
+        } else if (existingUser !== null) {
+            res.status(400);
+            return next(new Error("That username already exists!"));
+        }
+        const newUser = new User(req.body);
+        newUser.save((err, user) => {
+            if (err) return res.status(500).send({success: false, err});
+            const token = jwt.sign(user.toObject(), process.env.SECRET);
+            return res.status(201).send({ user: user.toObject(), token });
+        });
+    });
+});
+
+authRouter.post("/login", (req, res, next) => {
+    User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+        if (err) {
+            res.status(500);
+            return next(err);
+        } else if (!user || user.password !== req.body.password) {
+            res.status(403);
+            return next(new Error("Username or password are incorrect"));
+        }
+        const token = jwt.sign(user.toObject(), process.env.SECRET);
+        return res.send({ user: user.toObject(), token })
+    });
+});
+
+module.exports = authRouter;
